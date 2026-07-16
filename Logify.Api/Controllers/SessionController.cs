fac7b8942b4dd890b1687e5fe1eab2aac156f56e
MichaelLem <michaelthomaslem@gmail.com>
2026-07-16 11:49:57 -0700

    Updated SessionController

--- Logify.Api/Controllers/SessionController.cs
+++ Logify.Api/Controllers/SessionController.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Logify.BizLayer;
 
 namespace Logify.Api.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
+
     public class SessionController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
